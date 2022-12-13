08a6518b089b5a55240957854084f6a363b8da50
mamadou lamine ba <laminba2003@gmail.com>
2022-12-13 19:44:52 +0000

    improvements

--- src/main/java/com/spring/training/config/ApplicationConfig.java
+++ src/main/java/com/spring/training/config/ApplicationConfig.java
@@ -1,12 +1,16 @@
 package com.spring.training.config;
 
+import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.web.WebProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ClientHttpConnector;
@@ -14,15 +18,57 @@ import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.TrustManagerFactory;
+import java.security.KeyStore;
+
 @Configuration
+@Slf4j
 public class ApplicationConfig {
 
     @Bean
+    @Profile("!ssl")
     public WebClient webClient(ClientConfig config) {
         HttpClient httpClient = HttpClient.create()
-                .doOnConnected(connection ->  connection
-                .addHandlerLast(new ReadTimeoutHandler(10))
-                .addHandlerLast(new WriteTimeoutHandler(10)));
+                .doOnConnected(connection -> connection
+                        .addHandlerLast(new ReadTimeoutHandler(10))
+                        .addHandlerLast(new WriteTimeoutHandler(10)));
+
+        ClientHttpConnector connector = new ReactorClientHttpConnector(httpClient);
+
+        return webClientBuilder()
+                .baseUrl(config.getUrl())
+                .clientConnector(connector)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+
+    @Bean
+    @Profile("ssl")
+    public WebClient webSSLClient(ClientConfig config) {
+        HttpClient httpClient = HttpClient.create()
+                .doOnConnected(connection -> connection
+                        .addHandlerLast(new ReadTimeoutHandler(10))
+                        .addHandlerLast(new WriteTimeoutHandler(10)))
+                .secure(spec -> {
+                    try {
+                        KeyStore keyStore = KeyStore.getInstance("JKS");
+                        DefaultResourceLoader loader = new DefaultResourceLoader();
+                        String password = config.getSsl().get("key-store-password");
+                        String keystoreFile = config.getSsl().get("key-store");
+                        keyStore.load(loader.getResource(keystoreFile).getInputStream(), password.toCharArray());
+                        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+                        keyManagerFactory.init(keyStore, password.toCharArray());
+                        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+                        trustManagerFactory.init(keyStore);
+                        spec.sslContext(SslContextBuilder.forClient()
+                                .keyManager(keyManagerFactory)
+                                .trustManager(trustManagerFactory)
+                                .build());
+                    } catch (Exception e) {
+                        log.error("Unable to set SSL Context", e);
+                    }
+                });
 
         ClientHttpConnector connector = new ReactorClientHttpConnector(httpClient);
 
