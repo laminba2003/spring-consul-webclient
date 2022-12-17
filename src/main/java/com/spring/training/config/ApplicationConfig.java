e8220a05c4b25ab79f487257ffa3265d523e7fef
mamadou lamine ba <laminba2003@gmail.com>
2022-12-17 17:11:07 +0000

    improvements

--- src/main/java/com/spring/training/config/ApplicationConfig.java
+++ src/main/java/com/spring/training/config/ApplicationConfig.java
@@ -3,6 +3,7 @@ package com.spring.training.config;
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.web.WebProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -13,7 +14,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.client.reactive.ClientHttpConnector;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.http.codec.FormHttpMessageReader;
 import org.springframework.http.codec.ServerCodecConfigurer;
@@ -22,6 +22,7 @@ import org.springframework.web.reactive.config.WebFluxConfigurationSupport;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
+import reactor.netty.tcp.SslProvider;
 
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.TrustManagerFactory;
@@ -34,52 +35,28 @@ public class ApplicationConfig extends WebFluxConfigurationSupport {
     @Bean
     @Profile("!ssl")
     public WebClient webClient(ClientConfig config) {
-        HttpClient httpClient = HttpClient.create()
-                .doOnConnected(connection -> connection
-                        .addHandlerLast(new ReadTimeoutHandler(10))
-                        .addHandlerLast(new WriteTimeoutHandler(10)));
-        return buildWebClient(config, httpClient);
+        return buildWebClient(config, createHttpClient());
     }
 
     @Bean
     @Profile("ssl")
     public WebClient webSSLClient(ClientConfig config) {
-        HttpClient httpClient = HttpClient.create()
-                .doOnConnected(connection -> connection
-                        .addHandlerLast(new ReadTimeoutHandler(10))
-                        .addHandlerLast(new WriteTimeoutHandler(10)))
-                .secure(spec -> {
-                    try {
-                        String type = config.getSsl().get("key-store-type");
-                        KeyStore keyStore = KeyStore.getInstance(type);
-                        DefaultResourceLoader loader = new DefaultResourceLoader();
-                        String file = config.getSsl().get("key-store");
-                        String password = config.getSsl().get("key-store-password");
-                        keyStore.load(loader.getResource(file).getInputStream(), password.toCharArray());
-                        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-                        keyManagerFactory.init(keyStore, password.toCharArray());
-                        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-                        trustManagerFactory.init(keyStore);
-                        spec.sslContext(SslContextBuilder.forClient()
-                                .keyManager(keyManagerFactory)
-                                .trustManager(trustManagerFactory)
-                                .build());
-                    } catch (Exception e) {
-                        log.error("Unable to set SSL Context", e);
-                    }
-                });
-        return buildWebClient(config, httpClient);
+        return buildWebClient(config, createHttpClient().secure(spec -> configureSSL(spec, config)));
+    }
+
+    private HttpClient createHttpClient() {
+        return HttpClient.create().doOnConnected(connection -> connection
+                .addHandlerLast(new ReadTimeoutHandler(10))
+                .addHandlerLast(new WriteTimeoutHandler(10)));
     }
 
     private WebClient buildWebClient(ClientConfig config, HttpClient httpClient) {
-        ClientHttpConnector connector = new ReactorClientHttpConnector(httpClient);
         return webClientBuilder()
                 .baseUrl(config.getUrl())
-                .clientConnector(connector)
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .filter(new ServerBearerExchangeFilterFunction())
-                .exchangeStrategies(ExchangeStrategies.builder().codecs(c ->
-                        c.defaultCodecs().enableLoggingRequestDetails(true)).build()
-                ).defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .exchangeStrategies(ExchangeStrategies.builder().codecs(c -> c.defaultCodecs().enableLoggingRequestDetails(true)).build())
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
 
@@ -87,6 +64,24 @@ public class ApplicationConfig extends WebFluxConfigurationSupport {
     @LoadBalanced
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
+    }
+
+    @SneakyThrows
+    private void configureSSL(SslProvider.SslContextSpec spec, ClientConfig config) {
+        String type = config.getSsl().get("key-store-type");
+        KeyStore keyStore = KeyStore.getInstance(type);
+        DefaultResourceLoader loader = new DefaultResourceLoader();
+        String file = config.getSsl().get("key-store");
+        String password = config.getSsl().get("key-store-password");
+        keyStore.load(loader.getResource(file).getInputStream(), password.toCharArray());
+        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        keyManagerFactory.init(keyStore, password.toCharArray());
+        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        trustManagerFactory.init(keyStore);
+        spec.sslContext(SslContextBuilder.forClient()
+                .keyManager(keyManagerFactory)
+                .trustManager(trustManagerFactory)
+                .build());
     }
 
     @Bean
